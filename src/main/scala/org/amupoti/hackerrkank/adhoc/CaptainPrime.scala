fec0b65a5393d5c8b06161ba8d8ded4d9ddb1cf7
Marcel Arrufat <marrufat@zinio.com>
2016-12-14 17:34:08 +0100

    Remove unnecessary brackets

--- src/main/scala/org/amupoti/hackerrkank/adhoc/CaptainPrime.scala
+++ src/main/scala/org/amupoti/hackerrkank/adhoc/CaptainPrime.scala
@@ -11,21 +11,11 @@ object CaptainPrime {
     computeFate(iter.toList).foreach(println(_))
   }
 
-  def computeFate(persons: List[String]): List[String] = {
-    persons map computePosition
-  }
+  def computeFate(persons: List[String]): List[String] = persons map computePosition
 
-  def allLeftPrimes(p: String): Boolean = {
+  def allLeftPrimes(p: String): Boolean = 1 until p.length forall (c => isStringPrime(p.substring(c)))
 
-    1 until p.length forall (c => isStringPrime(p.substring(c)))
-
-  }
-
-  def allRightPrimes(p: String): Boolean = {
-
-    1 until p.length forall (c => isStringPrime(p.substring(0, p.length - c)))
-
-  }
+  def allRightPrimes(p: String): Boolean = 1 until p.length forall (c => isStringPrime(p.substring(0, p.length - c)))
 
 
   def computePosition(person: String): String = {
@@ -40,16 +30,10 @@ object CaptainPrime {
         case _ => "DEAD"
       }
     }
-
   }
 
+  def isPrime(n: Int): Boolean = n != 1 && (2 until n forall (n % _ != 0))
 
-  def isPrime(n: Int): Boolean = {
+  def isStringPrime(s: String): Boolean = isPrime(s.toInt)
 
-    n!=1 && (2 until n forall (n % _ != 0))
-  }
-
-  def isStringPrime(s: String): Boolean = {
-    isPrime(s.toInt)
-  }
 }
